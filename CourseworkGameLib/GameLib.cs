e7739b75e250bcafc85da9a1bc5cba05b24de2fc
Casey Tyler <keshakeshievich@gmail.com>
2025-01-22 19:48:52 +0700

    Добавьте файлы проекта.

---
+++ CourseworkGameLib/GameLib.cs
@@ -0,0 +1,17 @@
+﻿namespace CourseworkGameLib
+{
+    public class Persona1 //заготовка под персонажа 1
+    {
+
+    }
+
+    public class Persona2 //заготовка под персонажа 2
+    {
+
+    }
+
+    public class Persona3 //заготовка под персонажа 3
+    {
+
+    }
+}
